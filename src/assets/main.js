87e781b063d96a72ed1bfc9ee41212e88730916a
dwivedisarvesh <sarvesh.dwivedi2@dowjones.com>
2018-01-10 12:35:10 -0500

    new changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/970756.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCoures(response.courses.completed);
+    }
+  });
 
-  // your code will go here
+  function addCoures(courses) {
+
+    courses.forEach(function(course) {
+      console.log(course);
+      var $badges = $('#badges');
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />',{
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        'class': 'btn btn-primary',
+        target: '_blank',
+        text: 'See Course'
+      }).appendTo($course);
+
+    });
+  }
 
 });
